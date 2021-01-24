8a28a24c0ed33c73b2eb4dabe378470a51c0d1ba
KundaAstratta <krystof.humbert@gmail.com>
2021-01-24 17:44:54 +0100

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Christophe Humbert on 24/01/2021.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
