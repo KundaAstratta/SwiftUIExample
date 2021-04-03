a93d9d72c3bea54ac70b4ce187a480e5f15ffc72
KundaAstratta <krystof.humbert@gmail.com>
2021-04-04 00:04:24 +0200

    third commit with Hikes change

--- Landmarks/Views/ContentView.swift
+++ Landmarks/Views/ContentView.swift
@@ -9,45 +9,13 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-          MapView ()
-            .ignoresSafeArea(edges: .top)
-            .frame(height : 300)
-         
-          CircleImage()
-            .offset(y: -130)
-            .padding(.bottom , -130)
-            
-          VStack (alignment: .leading){
-            Text("La Roseraie")
-                    .font(.title)
-                    .foregroundColor(Color.black)
-            HStack {
-                Text("Parc départemental de la Roseraie")
-                    .font(.subheadline)
-                Spacer()
-                Text("Val de Marne")
-                    .font(.subheadline)
-            }
-            .font(.subheadline)
-            .foregroundColor(.secondary)
-            
-            Divider ()
-            
-            Text("About La Roseraie")
-                .font(.title2)
-            Text("Parc floral à thème.")
-            
-          }
-          .padding()
-            
-          Spacer()
-        }
+            LandmarkList()
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+            .environmentObject(ModelData())
     }
 }
