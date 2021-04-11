d257803916341b4a2ace983130f439838fa0fc34
KundaAstratta <krystof.humbert@gmail.com>
2021-04-11 23:38:36 +0200

    Complex Interfaces

--- Landmarks/Views/ContentView.swift
+++ Landmarks/Views/ContentView.swift
@@ -7,9 +7,38 @@
 
 import SwiftUI
 
+
 struct ContentView: View {
+    
+    @State private var selection: Tab = .featured
+
+
+    enum Tab {
+        case featured
+        case list
+        case hike
+    }
+
     var body: some View {
+        TabView(selection: $selection) {
+            CategoryHome()
+                .tabItem {
+                    Label("Featured", systemImage: "star")
+                }
+                .tag(Tab.featured)
+
             LandmarkList()
+                .tabItem {
+                    Label("List", systemImage: "list.bullet")
+                }
+                .tag(Tab.list)
+        
+            HikeView (hike: ModelData().hikes[0])
+                .tabItem {
+                    Label("Hike", systemImage: "person.crop.circle")
+                }
+                .tag(Tab.hike)
+        }
     }
 }
 
